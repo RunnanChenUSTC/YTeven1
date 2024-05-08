38b7d91f5f6a4e624fe63d334f25d093b7ddc3c2
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-05-08 13:23:07 +0000

    kfw

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -816,6 +816,7 @@ useEffect(() => {
       fetchQuestion(questionid).then(Content => {
         // 可以在这里使用获取到的问题内容
         const questionIdInt = parseInt(questionid, 10);
+        chatStore.newSession();
         doSubmit(decodeURIComponent(Content),questionIdInt);
         setAutoSubmitted(true);
         console.log('Fetched Content:', Content);
