66796c14a8fbc3fbab50b3c6ae9d781d3efe5742
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-05-08 15:50:19 +0000

    use delete all

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -818,7 +818,11 @@ useEffect(() => {
         // 可以在这里使用获取到的问题内容
         const questionIdInt = parseInt(questionid, 10);
         // console.log(firstQuestionIDReceived);
-        chatStore.deleteSession(chatStore.currentSessionIndex)
+        chatStore.updateCurrentSession(session => {
+          // 设置messages为空数组，从而删除所有消息
+          session.messages = [];
+          console.log("All messages have been deleted.");
+        });
         // chatStore.newSession();
         // if (!firstQuestionIDReceived) {
         //   setFirstQuestionIDReceived(true);
