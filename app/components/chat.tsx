def101718840dbcbc2136a957979019068189925
Chen Run Nan <129401246+RunnanChenUSTC@users.noreply.github.com>
2024-05-23 12:25:17 +0000

    debug URI decode bug

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -913,7 +913,8 @@ useEffect(() => {
         // } else {
         //   chatStore.deleteSession(chatStore.currentSessionIndex); 
         // }
-        doSubmit(decodeURIComponent(Content),questionIdInt);
+        // doSubmit(decodeURIComponent(Content),questionIdInt);
+        doSubmit(Content,questionIdInt);
         setAutoSubmitted(true);
         console.log('Fetched Content:', Content);
       });
