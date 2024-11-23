6b1f1bcb36a1030bf17b88310bcd0b0f380273cd
Chen Run Nan <SarahKaterina502@outlook.com>
2024-11-23 23:01:10 +0800

    gg

--- app/constant.ts
+++ app/constant.ts
@@ -50,8 +50,8 @@ export enum StoreKey {
 
 export const DEFAULT_SIDEBAR_WIDTH = 0;
 export const MAX_SIDEBAR_WIDTH = 500;
-export const MIN_SIDEBAR_WIDTH = 230;
-export const NARROW_SIDEBAR_WIDTH = 100;
+export const MIN_SIDEBAR_WIDTH = 0;
+export const NARROW_SIDEBAR_WIDTH = 0;
 
 export const ACCESS_CODE_PREFIX = "nk-";
 
